ed8d46c63ef29da9b22ccd785b41b1ea2e49755c
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-02-06 22:43:37 +0530

    cleanup and rust bug fix

--- Spotify-widget/src-tauri/src/main.rs
+++ Spotify-widget/src-tauri/src/main.rs
@@ -6,38 +6,10 @@ use std::io::Error;
 
 
 
-fn start_node_server() -> Result<(), Error> {
-    let _server = Command::new("node")
-        .arg("server.cjs")
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()?;
 
-    Ok(())
-}
 
-// fn read_file() -> String {
-//     let file_path = Path::new(
-//         "C:\\Users\\divya\\Documents\\GitHub\\Spotify-widget-tauri\\Spotify-widget\\src-tauri\\data.json"
-//     );
-//     let mut file = match File::open(&file_path) {
-//         Ok(file) => file,
-//         Err(e) => panic!("Failed to open file: {}", e),
-//     };
-//     let mut contents = String::new();
-//     if let Err(e) = file.read_to_string(&mut contents) {
-//         panic!("Failed to read file: {}", e);
-//     }
-//     return contents;
-// }
-// #[tauri::command]
-// fn get_access_token() ->String{
-//     let contents = read_file();
-//     println!("I was invoked from JS!");
-//     return contents;
-// }
 fn main() {
-    start_node_server().expect("Failed to start Node.js server");
+    
 
     // Read the contents of the file into a string
 
